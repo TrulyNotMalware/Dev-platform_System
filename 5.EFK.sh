67b51e3144ea065359917502aafd356ce9c1f9ae
TrulyNotMalware <notleebutyee@gmail.com>
2022-03-07 16:21:00 +0900

    Modify EFK yamls

--- 5.EFK.sh
+++ 5.EFK.sh
@@ -3,6 +3,7 @@
 #EFK 구축을 위한 shell scripts.
 ELASTIC_STORAGE_SIZE="10Gi"
 ELASTIC_PORT=9200
+ELASTIC_NODE_PORT=30920
 ELASTIC_ROOT_LOG_LEVEL=info
 KIBANA_CONTAINER_PORT=5601
 KIBANA_NODE_PORTS=30050
@@ -22,6 +23,7 @@ echo "###############"
 sed -e "s/ELASTIC_STORAGE_SIZE/${ELASTIC_STORAGE_SIZE}/g" < ${template_location}/elastic-search-template.yaml > yamls/created/elastic-search.yaml
 sed -i "s/ELASTIC_PORT$/${ELASTIC_PORT}/g" yamls/created/elastic-search.yaml
 sed -i "s/ELASTIC_ROOT_LOG_LEVEL/${ELASTIC_ROOT_LOG_LEVEL}/g" yamls/created/elastic-search.yaml
+sed -i "s/ELASTIC_NODE_PORT/${ELASTIC_NODE_PORT}/g" yamls/created/elastic-search.yaml
 sed -i "s/STORAGE_CLASS_NAME/${STORAGE_CLASS_NAME}/g" yamls/created/elastic-search.yaml
 
 sed -e "s/KIBANA_CONTAINER_PORT/${KIBANA_CONTAINER_PORT}/g" <${template_location}/kibana-template.yaml > yamls/created/kibana.yaml
